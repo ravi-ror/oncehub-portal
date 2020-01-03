a84a9f0fe89761fed179691c610be7ecfec0490d
Deepak Manwal <deepak.manwal@scheduleonce.com>
2020-01-03 16:23:27 +0530

    Country allowed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :about, :hobbies)
+    params.require(:user).permit(:first_name, :last_name, :about, :hobbies, :country)
   end
 
   def set_user
