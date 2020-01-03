4c8ee076e440da28d0891563af9c046a8e7076e2
Ravindra Rawat <ravindra.rawat@scheduleonce.com>
2020-01-03 14:46:36 +0530

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'recover-password' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home', :to => "static#index"
   root :to => "static#index"
 
   devise_scope :user do
     get 'profile', :to => "user#profile"
-    get 'login', to: 'devise/sessions#new'
-    get 'recover-password', to: 'devise/passwords#new'
-    delete 'logout', to: 'devise/sessions#destroy'
   end
 end
