bd1fc9a01e1567dbe79e1624856326e9c82b30a3
Deepak Manwal <deepak.manwal@scheduleonce.com>
2020-01-03 12:37:40 +0530

    Default users added in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+User.create(email: 'deepak@oncehub.com', password: 'oncehub1234')
+User.create(email: 'munmun@oncehub.com', password: 'oncehub1234')
+User.create(email: 'siddharth@oncehub.com', password: 'oncehub1234')
+User.create(email: 'ravi@oncehub.com', password: 'oncehub1234')
