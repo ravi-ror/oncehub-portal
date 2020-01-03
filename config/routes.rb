636f251edb1957883434a4a32b2d645a6d204c89
Ravindra Rawat <ravindra.rawat@scheduleonce.com>
2020-01-03 12:15:20 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home', :to => "static#index"
+  root :to => "static#index"
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'recover-password', to: 'devise/passwords#new'
+  end
+end
